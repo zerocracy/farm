b3b1b5bc6f5e58f40b564963d9679c4192380e1b
Yegor Bugayenko <yegor256@gmail.com>
2016-12-16 10:59:33 +0200

    #4 polish

--- src/main/java/com/zerocracy/farm/Main.java
+++ src/main/java/com/zerocracy/farm/Main.java
@@ -33,6 +33,7 @@ import org.takes.tk.TkText;
  * @author Yegor Bugayenko (yegor256@gmail.com)
  * @version $Id$
  * @since 0.1
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  */
 public final class Main {
 
