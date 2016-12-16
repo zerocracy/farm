f26d84de84335bb0e3b853b72117339580aeb3ba
Yegor Bugayenko <yegor256@gmail.com>
2016-12-16 13:01:36 +0200

    #4 show logs

--- src/main/java/com/zerocracy/crews/github/GithubCrew.java
+++ src/main/java/com/zerocracy/crews/github/GithubCrew.java
@@ -21,6 +21,7 @@ import com.jcabi.github.Github;
 import com.jcabi.github.RtPagination;
 import com.jcabi.http.Request;
 import com.jcabi.http.response.RestResponse;
+import com.jcabi.log.Logger;
 import com.zerocracy.jstk.Crew;
 import com.zerocracy.jstk.Farm;
 import com.zerocracy.jstk.Project;
@@ -69,6 +70,7 @@ public final class GithubCrew implements Crew {
             )
             .map(json -> new Event(this.github, json))
             .collect(Collectors.toList());
+        events.forEach(event -> Logger.info(this, "%s", event));
         for (final Event event : events) {
             GithubCrew.employ(farm, event);
         }
