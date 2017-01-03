5a6872892968858c64c6bd9ddd8a66bec192d7d5
Yegor Bugayenko <yegor256@gmail.com>
2017-01-03 16:05:55 +0200

    #6 ReFarewell

--- src/main/java/com/zerocracy/crews/slack/SkCrew.java
+++ src/main/java/com/zerocracy/crews/slack/SkCrew.java
@@ -19,6 +19,7 @@ package com.zerocracy.crews.slack;
 import com.jcabi.log.Logger;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.events.SlackChannelJoined;
+import com.ullink.slack.simpleslackapi.events.SlackChannelLeft;
 import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
 import com.zerocracy.jstk.Crew;
@@ -63,6 +64,13 @@ public final class SkCrew implements Crew {
      */
     private static final Reaction<SlackChannelJoined> JOINED = new ReLogged<>(
         new ReInvite()
+    );
+
+    /**
+     * When joined new channel.
+     */
+    private static final Reaction<SlackChannelLeft> LEFT = new ReLogged<>(
+        new ReFarewell()
     );
 
     /**
@@ -119,6 +127,15 @@ public final class SkCrew implements Crew {
             (event, sess) -> {
                 try {
                     SkCrew.JOINED.react(farm, event, ssn);
+                } catch (final IOException ex) {
+                    throw new IllegalStateException(ex);
+                }
+            }
+        );
+        ssn.addChannelLeftListener(
+            (event, sess) -> {
+                try {
+                    SkCrew.LEFT.react(farm, event, ssn);
                 } catch (final IOException ex) {
                     throw new IllegalStateException(ex);
                 }
