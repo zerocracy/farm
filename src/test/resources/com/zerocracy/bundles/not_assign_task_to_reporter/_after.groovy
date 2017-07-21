fb593923fb819a082ba32940cd5327fe509993e8
g4s8 <g4s8.public@gmail.com>
2017-07-21 10:36:58 +0400

    #83 - fixed todo syntax

--- src/test/resources/com/zerocracy/bundles/not_assign_task_to_reporter/_after.groovy
+++ src/test/resources/com/zerocracy/bundles/not_assign_task_to_reporter/_after.groovy
@@ -20,7 +20,7 @@ import com.jcabi.xml.XML
 import com.zerocracy.jstk.Project
 import com.zerocracy.pm.staff.Elections
 /**
- * @todo 83:30min We should add new "bans" to this XML document every time
+ * @todo #83:30min We should add new "bans" to this XML document every time
  *  we add a new job to the WBS. Lets create a new stakeholder
  *  pm.staff.bans.ban_github_reporter. It has to catch "Job was added to WBS"
  *  type, check whether the job starts with gh: and then take the author of
