deb3d27c6991d9e743707e1602893a4d5dc97863
Yegor Bugayenko <yegor256@gmail.com>
2017-08-02 16:46:47 +0300

    #114 invitation to join

--- src/main/resources/com/zerocracy/stk/pm/in/orders/start_order.groovy
+++ src/main/resources/com/zerocracy/stk/pm/in/orders/start_order.groovy
@@ -22,6 +22,7 @@ import com.zerocracy.jstk.Project
 import com.zerocracy.pm.ClaimIn
 import com.zerocracy.pm.ClaimOut
 import com.zerocracy.pm.in.Orders
+import com.zerocracy.pm.staff.Roles
 
 def exec(Project project, XML xml) {
   new Assume(project, xml).type('Start order')
@@ -39,13 +40,16 @@ def exec(Project project, XML xml) {
     ).postTo(project)
   } else {
     orders.assign(job, login, reason)
-    claim.reply(
-      String.format(
-        'Job `%s` assigned to @%s, please go ahead' +
-        ' ([policy](http://datum.zerocracy.com/pages/policy.html)).',
-        job, login
-      )
-    ).postTo(project)
+    String msg = "Job `${job}` assigned to @${login}, please go ahead" +
+      ' ([policy](http://datum.zerocracy.com/pages/policy.html)).'
+    if (!new Roles(project).bootstrap().hasAnyRole(login)) {
+      msg += " @${login} is not a member of this project yet," +
+        ' but can request to join.' +
+        " @${login} check your" +
+        " [Zerocracy profile](http://www.0crat.com/u/${login})" +
+        ' and follow the instructions.'
+    }
+    claim.reply(msg).postTo(project)
     new ClaimOut()
       .type('Order was given')
       .param('job', job)
