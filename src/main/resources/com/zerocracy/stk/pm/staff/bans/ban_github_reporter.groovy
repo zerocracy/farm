9e2c2896e613e158222a84aa3d7e55a45e03e041
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2017-08-07 10:47:43 +0800

    #93 Add issue reporter to Bans

---
+++ src/main/resources/com/zerocracy/stk/pm/staff/bans/ban_github_reporter.groovy
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2016-2017 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.stk.pm.scope.wbs
+
+import com.jcabi.github.Github
+import com.jcabi.github.Issue
+import com.jcabi.xml.XML
+import com.zerocracy.farm.Assume
+import com.zerocracy.jstk.Project
+import com.zerocracy.pm.ClaimIn
+import com.zerocracy.pm.staff.Bans
+import com.zerocracy.radars.github.Job
+
+def exec(Project project, XML xml) {
+  new Assume(project, xml).type('Job was added to WBS')
+  ClaimIn claim = new ClaimIn(xml)
+  String job = claim.param('job')
+  if (!job.startsWith('gh:')) {
+    return
+  }
+  Github github = binding.variables.github
+  Issue.Smart issue = new Issue.Smart(new Job.Issue(github, job))
+  String author = issue.author().login().toLowerCase(Locale.ENGLISH)
+  new Bans(project).bootstrap()
+      .ban(job, author, "This user reported the ticket")
+}
