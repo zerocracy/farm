f5eebd27e3922b45bab9b36401349d968f3df3c2
Yegor Bugayenko <yegor256@gmail.com>
2017-08-11 10:30:46 +0300

    #193 deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ trap 'git reset HEAD~1 && rm settings.xml' EXIT
 cp /code/home/assets/zerocracy/settings.xml .
 git add settings.xml
 git commit -m 'settings.xml for heroku'
-sed -i "s/\${buildNumber}/${head}/g" ./src/main/resources/main.properties
+sed -i "s/\${buildNumber}/${head}/g" src/main/resources/main.properties
+git add src/main/resources/main.properties
 git commit -m 'build.revision updated'
 git push heroku master -f
 
