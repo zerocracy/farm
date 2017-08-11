f249a7495ebcbeec9728f0aa78f0a3101088959f
Yegor Bugayenko <yegor256@gmail.com>
2017-08-11 10:30:04 +0300

    #193 deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+head=$(git rev-parse --short HEAD)
 cd $(dirname $0)
-#mvn clean test
+trap 'git reset HEAD~1 && rm settings.xml' EXIT
 cp /code/home/assets/zerocracy/settings.xml .
 git add settings.xml
 git commit -m 'settings.xml for heroku'
-trap 'git reset HEAD~1 && rm settings.xml' EXIT
+sed -i "s/\${buildNumber}/${head}/g" ./src/main/resources/main.properties
+git commit -m 'build.revision updated'
 git push heroku master -f
 
