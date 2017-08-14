851ed16a775437e49c4371aaefc73e954ac18b73
Yegor Bugayenko <yegor256@gmail.com>
2017-08-14 17:01:25 +0300

    #205 small optimization

--- src/main/java/com/zerocracy/farm/S3Item.java
+++ src/main/java/com/zerocracy/farm/S3Item.java
@@ -120,7 +120,7 @@ final class S3Item implements Item {
 
     @Override
     public void close() throws IOException {
-        if (this.open.get() && (!this.ocket.exists() || this.dirty())) {
+        if (this.open.get() && (this.dirty() || !this.ocket.exists())) {
             final ObjectMetadata meta = new ObjectMetadata();
             final long start = System.currentTimeMillis();
             meta.setContentLength(this.temp.toFile().length());
