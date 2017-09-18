6d9d0a273f250ffec3d901d65fbac1ced08aeee0
g4s8 <g4s8.public@gmail.com>
2017-09-18 23:11:07 +0400

    #137 - reminders todo

---
+++ src/test/java/com/zerocracy/pm/time/RemindersTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2016-2017 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.pm.time;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test case for reminders.
+ *
+ * @author Kirill (g4s8.public@gmail.com)
+ * @version $Id$
+ * @since 0.17
+ * @todo #137:30m Add new stakeholder, which will go through all orders
+ *  and check their age. If they are old enough to create
+ *  a new reminder (5 days, 8 days) - we should create it and send
+ *  a notification to the user that the task will be taken away
+ *  from him in 10 days (this is our policy).
+ * @checkstyle JavadocMethodCheck (500 lines)
+ */
+public final class RemindersTest {
+
+    @Test
+    @Ignore
+    public void allReminders() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+}
