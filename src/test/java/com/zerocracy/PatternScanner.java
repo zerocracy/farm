a3dfac46c89b8006d3ecdc17368f79b2b4892fcc
g4s8 <g4s8.public@gmail.com>
2017-09-30 15:30:08 +0400

    #201 - fixed bundle resources scanning

---
+++ src/test/java/com/zerocracy/PatternScanner.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2016-2017 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Multimap;
+import java.util.regex.Pattern;
+import org.reflections.Configuration;
+import org.reflections.scanners.Scanner;
+import org.reflections.vfs.Vfs;
+
+/**
+ * Resource scanner which filter file names by pattern.
+ * @author Kirill (g4s8.public@gmail.com)
+ * @version $Id$
+ * @since 0.17
+ */
+public final class PatternScanner implements Scanner {
+
+    /**
+     * Origin scanner.
+     */
+    private final Scanner origin;
+
+    /**
+     * Pattern for files.
+     */
+    private final Pattern pattern;
+
+    /**
+     * Ctor.
+     * @param org Origin scanner
+     * @param ptn Pattern for files.
+     */
+    public PatternScanner(final Scanner org, final Pattern ptn) {
+        this.origin = org;
+        this.pattern = ptn;
+    }
+
+    @Override
+    public boolean acceptsInput(final String file) {
+        return this.pattern.matcher(file).matches()
+            && this.origin.acceptsInput(file);
+    }
+
+    @Override
+    public void setConfiguration(final Configuration configuration) {
+        this.origin.setConfiguration(configuration);
+    }
+
+    @Override
+    public Multimap<String, String> getStore() {
+        return this.origin.getStore();
+    }
+
+    @Override
+    public void setStore(final Multimap<String, String> store) {
+        this.origin.setStore(store);
+    }
+
+    @Override
+    public Scanner filterResultsBy(final Predicate<String> filter) {
+        return this.origin.filterResultsBy(filter);
+    }
+
+    @Override
+    public Object scan(final Vfs.File file, final Object obj) {
+        return this.origin.scan(file, obj);
+    }
+
+    @Override
+    public boolean acceptResult(final String fqn) {
+        return this.origin.acceptResult(fqn);
+    }
+}
