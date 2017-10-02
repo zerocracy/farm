2a8402cb99b80b268416810cdad79b6dcacccd3f
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2017-10-02 15:14:20 +0800

    #237 Qulice fixes

--- src/test/resources/com/zerocracy/bundles/assigns_to_github_assignee/_before.groovy
+++ src/test/resources/com/zerocracy/bundles/assigns_to_github_assignee/_before.groovy
@@ -2,11 +2,11 @@
  * Copyright (c) 2016-2017 Zerocracy
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to read
+ * of this software and associated documentation files (the 'Software'), to read
  * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
  * merge, publish, distribute, sublicense, and/or sell copies of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -29,16 +29,12 @@ import javax.json.Json
 
 def exec(Project project, XML xml) {
   Github github = binding.variables.github
-  def repo = github.repos().create(new Repos.RepoCreate("test", false))
+  def repo = github.repos().create(new Repos.RepoCreate('test', false))
   def issue =
-      new Issue.Smart(repo.issues().create("hello, world", ""))
-  issue.assign("yegor256")
+      new Issue.Smart(repo.issues().create('hello, world', ''))
+  issue.assign('yegor256')
   repo.issueEvents()
-    .create(Event.ASSIGNED, issue.number(), "yegor256", com.google.common.base.Optional.absent())
-  final xpath = String.format(
-      "links/link[@rel='github' and @href='%s']",
-      repo.coordinates().toString().toLowerCase(Locale.ENGLISH)
-  )
+    .create(Event.ASSIGNED, issue.number(), 'yegor256', com.google.common.base.Optional.absent())
   new RbOnAssign().react(
     new FkFarm(project),
     github,
