520145a232a1651b23fbfe7f1aaa816fced7ede1
Yegor Bugayenko <yegor256@gmail.com>
2017-10-04 11:20:40 +0300

    fix

--- src/main/java/com/zerocracy/radars/github/ReIfAddressed.java
+++ src/main/java/com/zerocracy/radars/github/ReIfAddressed.java
@@ -47,20 +47,27 @@ public final class ReIfAddressed implements Response {
     @Override
     public boolean react(final Farm farm, final Comment.Smart comment)
         throws IOException {
-        final Pattern pattern = Pattern.compile(
-            String.format(
-                "@%s .+",
-                comment.issue().repo().github().users().self().login()
-            ),
-            Pattern.MULTILINE | Pattern.CASE_INSENSITIVE
+        final String self = String.format(
+            "@%s", comment.issue().repo().github().users().self().login()
         );
-        if (!pattern.matcher(comment.body()).matches()) {
-            throw new SoftException(
-                // @checkstyle LineLength (1 line)
-                "Are you speaking to me or about me? You must always start your message with my name if you want to address it to me."
+        boolean done = false;
+        if (comment.body().contains(self)) {
+            final Pattern pattern = Pattern.compile(
+                String.format(
+                    "@%s .+",
+                    comment.issue().repo().github().users().self().login()
+                ),
+                Pattern.MULTILINE | Pattern.CASE_INSENSITIVE
             );
+            if (!pattern.matcher(comment.body()).matches()) {
+                throw new SoftException(
+                    // @checkstyle LineLength (1 line)
+                    "Are you speaking to me or about me? You must always start your message with my name if you want to address it to me."
+                );
+            }
+            done = this.origin.react(farm, comment);
         }
-        return this.origin.react(farm, comment);
+        return done;
     }
 
 }
