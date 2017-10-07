fa078c8a2edcc3e1bd78e50e11ad591c86aa3bc1
Yegor Bugayenko <yegor256@gmail.com>
2017-10-07 22:45:33 +0300

    typo

--- src/main/java/com/zerocracy/entry/Main.java
+++ src/main/java/com/zerocracy/entry/Main.java
@@ -21,6 +21,7 @@ import com.zerocracy.farm.S3Farm;
 import com.zerocracy.farm.SmartFarm;
 import com.zerocracy.farm.footprint.FtFarm;
 import com.zerocracy.farm.props.Props;
+import com.zerocracy.farm.props.PropsFarm;
 import com.zerocracy.jstk.Farm;
 import com.zerocracy.radars.github.GithubRoutine;
 import com.zerocracy.radars.github.TkGithub;
@@ -81,7 +82,7 @@ public final class Main {
         }
         Sentry.init(props.get("//sentry/dsn", ""));
         final Farm farm = new SmartFarm(
-            new FtFarm(new S3Farm(new ExtBucket().value()))
+            new FtFarm(new PropsFarm(new S3Farm(new ExtBucket().value())))
         ).value();
         try (
             final SlackRadar radar = new SlackRadar(farm);
