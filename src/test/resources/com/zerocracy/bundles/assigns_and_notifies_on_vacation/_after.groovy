d0fe79f54bd2bbe465233dfb350acaa4d052cd19
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2017-10-11 10:08:40 +0800

    #240 Complain about vacation status

---
+++ src/test/resources/com/zerocracy/bundles/assigns_and_notifies_on_vacation/_after.groovy
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2016-2017 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.bundles.assigns_performer
+
+import com.jcabi.github.Comment
+import com.jcabi.github.Coordinates
+import com.jcabi.github.Github
+import com.jcabi.xml.XML
+import com.zerocracy.entry.ExtGithub
+import com.zerocracy.jstk.Farm
+import com.zerocracy.jstk.Project
+import com.zerocracy.pm.in.Orders
+
+def exec(Project project, XML xml) {
+  def orders = new Orders(project).bootstrap()
+  assert orders.performer('gh:test/test#1') == 'yegor256'
+  Github github = new ExtGithub(binding.variables.farm).value()
+  def repo = github.repos().get(new Coordinates.Simple('test/test'))
+  def issue = repo.issues().get(1)
+  assert new Comment.Smart(
+    issue.comments().iterate(new Date()).iterator().next()
+  ).body().contains('@yegor256 is on vacation')
+}
