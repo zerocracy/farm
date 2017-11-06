f4df47465c63f0c2b945a4339e1e460aaf3599b7
g4s8 <g4s8.public@gmail.com>
2017-11-06 19:52:05 +0400

    zerocracy/datum#112 - cancel order

--- src/main/resources/com/zerocracy/stk/pm/time/reminders/resign_reminder.groovy
+++ src/main/resources/com/zerocracy/stk/pm/time/reminders/resign_reminder.groovy
@@ -19,14 +19,10 @@ def exec(Project project, XML xml) {
   )
   def orders = new Orders(project).bootstrap()
   orders.olderThan(claimTime.minusDays(10)).forEach {
-    def login = orders.performer(it)
-    orders.resign(it)
     new ClaimOut()
-      .type('Notify job')
+      .type('Cancel order')
       .token("job;$it")
-      .param(
-      'message',
-      "@$login this job was resigned from you after 10 days (this is our policy)."
-    ).postTo(project)
+      .param('job', it)
+      .postTo(project)
   }
 }
