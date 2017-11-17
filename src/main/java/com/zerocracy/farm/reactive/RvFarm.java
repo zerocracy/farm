f7cf8f749adcbca0699c3748f5f758b231043ccb
Yegor Bugayenko <yegor256@gmail.com>
2017-11-17 12:04:36 +0300

    #342 typo

--- src/main/java/com/zerocracy/farm/reactive/RvFarm.java
+++ src/main/java/com/zerocracy/farm/reactive/RvFarm.java
@@ -161,6 +161,7 @@ public final class RvFarm implements Farm {
                         )
                     )
                 )
+                .up()
                 .add("locks")
                 .append(
                     new Joined<Directive>(
