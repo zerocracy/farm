b9124bb103a017f8ae0dda49bcf79f3583912959
g4s8 <g4s8.public@gmail.com>
2018-01-03 23:05:33 +0400

    #245 - impediments

---
+++ src/main/resources/com/zerocracy/stk/pm/in/impediments/register_impediment.groovy
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.stk.pm.in.impediments
+
+import com.jcabi.xml.XML
+import com.zerocracy.farm.Assume
+import com.zerocracy.jstk.Project
+import com.zerocracy.pm.ClaimIn
+import com.zerocracy.pm.ClaimOut
+import com.zerocracy.pm.in.Impediments
+
+
+def exec(Project project, XML xml) {
+  new Assume(project, xml).notPmo()
+  new Assume(project, xml).type('Register impediment')
+
+  ClaimIn claim = new ClaimIn(xml)
+  String job = claim.param('job')
+
+  new Impediments(project)
+    .bootstrap()
+    .register(job, 'The user just asked to wait a bit.')
+
+  new ClaimOut()
+    .type('Notify job')
+    .token("job;${job}")
+    .param('message', 'impediment was registered successfully')
+    .postTo(project)
+}
