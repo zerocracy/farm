46f6603100470fc585a0e57dc3c6f37e823bbdc9
Yegor Bugayenko <yegor256@gmail.com>
2018-01-08 16:55:47 +0300

    #372 0.51.1

--- src/main/java/com/zerocracy/Xocument.java
+++ src/main/java/com/zerocracy/Xocument.java
@@ -69,7 +69,7 @@ public final class Xocument {
     /**
      * Current DATUM version.
      */
-    public static final String VERSION = "0.51";
+    public static final String VERSION = "0.51.1";
 
     /**
      * Cache of documents.
