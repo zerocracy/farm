381b1bdb88aae26cfabc63192f2ded02fd083cb6
Yegor Bugayenko <yegor256@gmail.com>
2018-01-08 14:48:45 +0300

    #375 better messages

--- src/main/resources/com/zerocracy/stk/pm/in/orders/start_order.groovy
+++ src/main/resources/com/zerocracy/stk/pm/in/orders/start_order.groovy
@@ -23,6 +23,7 @@ import com.zerocracy.pm.ClaimIn
 import com.zerocracy.pm.ClaimOut
 import com.zerocracy.pm.cost.Ledger
 import com.zerocracy.pm.in.Orders
+import com.zerocracy.pm.scope.Wbs
 import com.zerocracy.pm.staff.Roles
 import com.zerocracy.pmo.People
 
@@ -38,11 +39,28 @@ def exec(Project project, XML xml) {
     return
   }
   orders.assign(job, login, reason)
-  String msg = "Job `${job}` assigned to @${login} " +
-    " ([profile](http://www.0crat.com/u/${login}}))." +
-    ' The budget is [fixed](http://datum.zerocracy.com/pages/policy.html#4)' +
-    ' and it is 30 minutes. Please, read the' +
-    ' [Policy](http://datum.zerocracy.com/pages/policy.html) and go ahead.'
+  String role = new Wbs(project).bootstrap().role(job)
+  String msg
+  if (role == 'REV') {
+    String arc = new Roles(project).bootstrap().findByRole('ARC')[0]
+    msg = "This PR `${job}` assigned to @${login} " +
+      " ([profile](http://www.0crat.com/u/${login}}))." +
+      ' The budget is [fixed](http://datum.zerocracy.com/pages/policy.html#4)' +
+      ' and it is 15 minutes. Please, read' +
+      ' [§27](http://datum.zerocracy.com/pages/policy.html#27)' +
+      ' and [§10](http://datum.zerocracy.com/pages/policy.html#10).'
+      " If and when you decide to accept the changes, inform @${arc} right in this ticket."
+  } else {
+    msg = "Job `${job}` assigned to @${login} " +
+      " ([profile](http://www.0crat.com/u/${login}}))." +
+      ' The budget is [fixed](http://datum.zerocracy.com/pages/policy.html#4)' +
+      ' and it is 30 minutes. Please, read' +
+      ' [§4](http://datum.zerocracy.com/pages/policy.html#4),' +
+      ' [§8](http://datum.zerocracy.com/pages/policy.html#8),' +
+      ' and [§9](http://datum.zerocracy.com/pages/policy.html#9).' +
+      ' If the task is not clear, read [this](http://www.yegor256.com/2015/02/16/it-is-not-a-school.html)' +
+      ' and [this](http://www.yegor256.com/2015/01/15/how-to-cut-corners.html).'
+  }
   if (!new Roles(project).bootstrap().hasAnyRole(login)) {
     msg += " @${login} is not a member of this project yet," +
       ' but can request to join.' +
