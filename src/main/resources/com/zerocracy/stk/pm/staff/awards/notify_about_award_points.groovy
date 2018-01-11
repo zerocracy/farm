7b0ecb33fcbbf5bb2b3eb4a86afbfd951a98877e
Yegor Bugayenko <yegor256@gmail.com>
2018-01-11 15:14:25 +0300

    #372 ignore irrelevant roles

--- src/main/resources/com/zerocracy/stk/pm/staff/awards/notify_about_award_points.groovy
+++ src/main/resources/com/zerocracy/stk/pm/staff/awards/notify_about_award_points.groovy
@@ -19,6 +19,7 @@ package com.zerocracy.stk.pm.staff.awards
 import com.jcabi.xml.XML
 import com.zerocracy.Par
 import com.zerocracy.farm.Assume
+import com.zerocracy.jstk.Farm
 import com.zerocracy.jstk.Project
 import com.zerocracy.pm.ClaimIn
 import com.zerocracy.pm.ClaimOut
@@ -33,12 +34,14 @@ def exec(Project project, XML xml) {
   Integer points = Integer.parseInt(claim.param('points'))
   Awards awards = new Awards(project, login).bootstrap()
   String reason = claim.param('reason')
+  Farm farm = binding.variables.farm
   new ClaimOut()
     .type('Notify user')
     .token("user;${login}")
     .param(
       'message',
       new Par(
+        farm,
         'You got %+d point(s) in the job %s in %s,',
         'your total is [%+d](/u/%s/awards), see §18: %s'
       ).say(points, job, project.pid(), awards.total(), login, reason)
