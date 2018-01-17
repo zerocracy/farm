1f97b89cbb521a263566246bb001fc1236f06be3
Carlos Miranda <carlos.m.a.miranda@accenture.com>
2018-01-17 11:30:59 +0800

    #390 Reset scores for people with score below -200

---
+++ src/main/resources/com/zerocracy/stk/pm/staff/awards/reject_people_below_score.groovy
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.stk.pm.staff.awards
+
+import com.jcabi.xml.XML
+import com.zerocracy.farm.Assume
+import com.zerocracy.Project
+import com.zerocracy.pm.ClaimIn
+import com.zerocracy.pm.ClaimOut
+import com.zerocracy.pmo.Awards
+
+def exec(Project project, XML xml) {
+  new Assume(project, xml).notPmo()
+  new Assume(project, xml).type('Award points were added')
+  ClaimIn claim = new ClaimIn(xml)
+  String login = claim.param('login')
+  Integer points = Integer.parseInt(claim.param('points'))
+  Awards awards = new Awards(project, login).bootstrap()
+  if (awards.total() <= -200) {
+    // @todo 390:30min We should remove people from people.xml
+    //  when their score goes below 200. Let's implement that.
+    //  We should also notify the person in case that happens.
+    String job = claim.param('job')
+    new ClaimOut()
+      .type('Award points were added')
+      .param('job', job)
+      .param('login', login)
+      .param('points', awards.total())
+      .param(
+        'reason',
+        'Your score is too low. Adding back points to reset.'
+       )
+      .postTo(project)
+  }
+}
