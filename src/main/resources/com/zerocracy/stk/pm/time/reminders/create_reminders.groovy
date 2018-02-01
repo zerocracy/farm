2f49154527bd3f00c6081fe18fcf19aed38abd06
Yegor Bugayenko <yegor256@gmail.com>
2018-02-01 16:37:02 +0300

    #493 no reminding if impediments exist

--- src/main/resources/com/zerocracy/stk/pm/time/reminders/create_reminders.groovy
+++ src/main/resources/com/zerocracy/stk/pm/time/reminders/create_reminders.groovy
@@ -21,6 +21,7 @@ import com.zerocracy.farm.Assume
 import com.zerocracy.Project
 import com.zerocracy.pm.ClaimIn
 import com.zerocracy.pm.ClaimOut
+import com.zerocracy.pm.in.Impediments
 import com.zerocracy.pm.in.Orders
 import com.zerocracy.pm.scope.Wbs
 import com.zerocracy.pm.time.Reminders
@@ -45,9 +46,13 @@ def exec(Project project, XML xml) {
   }
   Map<String, String> expired = orders.reminders(5)
     .plus(orders.reminders(8))
+  Impediments impediments = new Impediments(project).bootstrap()
   for (Map.Entry<String, String> entry : expired.entrySet()) {
     String job = entry.key
     if (wbs.role(job) == 'REV') {
+      continue
+    }
+    if (impediments.exists(job)) {
       continue
     }
     String label = entry.value
