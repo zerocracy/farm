e94d66a939f05c55cec52c6252f35933df880b23
g4s8 <g4s8.public@gmail.com>
2018-02-17 12:37:29 +0300

    #478 - remove 'scope' label when removed from WBS

--- src/main/resources/com/zerocracy/stk/pm/scope/wbs/remove_label_from_github_ticket.groovy
+++ src/main/resources/com/zerocracy/stk/pm/scope/wbs/remove_label_from_github_ticket.groovy
@@ -14,46 +14,34 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.zerocracy.bundles.adds_label_to_issue_in_wbs
+package com.zerocracy.stk.pm.scope.wbs
 
 import com.jcabi.github.Github
 import com.jcabi.github.Issue
 import com.jcabi.github.IssueLabels
+import com.jcabi.log.Logger
 import com.jcabi.xml.XML
 import com.zerocracy.Farm
 import com.zerocracy.Project
 import com.zerocracy.entry.ExtGithub
-import com.zerocracy.pm.scope.Wbs
+import com.zerocracy.farm.Assume
+import com.zerocracy.pm.ClaimIn
 import com.zerocracy.radars.github.Job
-import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers
 
 def exec(Project project, XML xml) {
-  def wbs = new Wbs(project).bootstrap()
-  String wbsIn = 'gh:test/test#1'
-  String wbsOut = 'gh:test/test#2'
-  MatcherAssert.assertThat(
-    'issue #1 is not in WBS',
-    wbs.exists(wbsIn),
-    Matchers.is(true)
-  )
-  MatcherAssert.assertThat(
-    'issue #2 in WBS',
-    wbs.exists(wbsOut),
-    Matchers.is(false)
-  )
+  new Assume(project, xml).notPmo()
+  new Assume(project, xml).type('Job removed from WBS')
+  ClaimIn claim = new ClaimIn(xml)
+  String job = claim.param('job')
+  if (!job.startsWith('gh:')) {
+    return
+  }
   Farm farm = binding.variables.farm
   Github github = new ExtGithub(farm).value()
-  Issue.Smart issueIn = new Issue.Smart(new Job.Issue(github, wbsIn))
-  Issue.Smart issueOut = new Issue.Smart(new Job.Issue(github, wbsOut))
-  MatcherAssert.assertThat(
-    'Issue in WBS does not contain "scope" label',
-    new IssueLabels.Smart(issueIn.labels()).contains('scope'),
-    Matchers.is(true)
-  )
-  MatcherAssert.assertThat(
-    'Issue not in WBS contains "scope" label',
-    new IssueLabels.Smart(issueOut.labels()).contains('scope'),
-    Matchers.is(false)
-  )
+  Issue.Smart issue = new Issue.Smart(new Job.Issue(github, job))
+  try {
+    new IssueLabels.Smart(issue.labels()).removeIfExists('scope')
+  } catch (AssertionError ex) {
+    Logger.warn(this, "Can't remove label from issue %s: %s", issue, ex.localizedMessage)
+  }
 }
