f0de45ae8333cbf2c691c2624457361faa670222
Yegor Bugayenko <yegor256@gmail.com>
2018-03-01 17:47:25 +0300

    #684 fixed

--- src/main/resources/com/zerocracy/stk/pmo/awards/penalize_for_same_user_github_assignment.groovy
+++ src/main/resources/com/zerocracy/stk/pmo/awards/penalize_for_same_user_github_assignment.groovy
@@ -45,19 +45,19 @@ def exec(Project project, XML xml) {
   Farm farm = binding.variables.farm
   Github github = new ExtGithub(farm).value()
   Issue.Smart issue = new Issue.Smart(new Job.Issue(github, job))
-  String author = claim.author()
-  if (issue.author().login().equalsIgnoreCase(author)) {
+  String assignee = claim.param('login')
+  if (issue.author().login().equalsIgnoreCase(assignee)) {
     claim.copy()
       .type('Make payment')
       .param('job', job)
-      .param('login', author)
+      .param('login', claim.author())
       .param(
-        'reason',
-        new Par(
-          'It is strongly discouraged',
-          'to assign jobs to their creators, see §19'
-        ).say()
-      )
+      'reason',
+      new Par(
+        'It is strongly discouraged',
+        'to assign jobs to their creators, see §19'
+      ).say()
+    )
       .param('minutes', new Policy().get('19.self-penalty', -15))
       .postTo(project)
   }
