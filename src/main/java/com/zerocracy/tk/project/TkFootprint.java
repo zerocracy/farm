d45c91c0e6b35d51a9bb1bb440ac6a7c2f61c579
Yegor Bugayenko <yegor256@gmail.com>
2018-03-07 22:22:59 +0300

    #751 download text/json footprint

--- src/main/java/com/zerocracy/tk/project/TkFootprint.java
+++ src/main/java/com/zerocracy/tk/project/TkFootprint.java
@@ -17,6 +17,9 @@
 package com.zerocracy.tk.project;
 
 import com.jcabi.log.Logger;
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
+import com.jcabi.xml.XSLDocument;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Sorts;
@@ -28,21 +31,28 @@ import com.zerocracy.tk.RsPage;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 import org.bson.Document;
+import org.cactoos.Scalar;
 import org.cactoos.list.SolidList;
 import org.cactoos.map.MapEntry;
 import org.cactoos.map.MapOf;
+import org.cactoos.scalar.IoCheckedScalar;
 import org.takes.Response;
 import org.takes.facets.fork.RqRegex;
 import org.takes.facets.fork.TkRegex;
 import org.takes.rq.RqHref;
+import org.takes.rs.RsWithBody;
+import org.takes.rs.RsWithType;
 import org.takes.rs.xe.XeAppend;
 import org.takes.rs.xe.XeChain;
 import org.takes.rs.xe.XeLink;
 import org.takes.rs.xe.XeSource;
 import org.takes.rs.xe.XeTransform;
 import org.takes.rs.xe.XeWhen;
+import org.xembly.Directives;
+import org.xembly.Xembler;
 
 /**
  * Footprint page.
@@ -51,14 +61,17 @@ import org.takes.rs.xe.XeWhen;
  * @version $Id$
  * @since 0.12
  * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ * @checkstyle ExecutableStatementCountCheck (500 lines)
  */
-@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+@SuppressWarnings
+    (
+        {
+            "PMD.AvoidDuplicateLiterals",
+            "PMD.ExcessiveMethodLength",
+            "PMD.ExcessiveImports"
+        }
+    )
 public final class TkFootprint implements TkRegex {
-
-    /**
-     * How much per page.
-     */
-    private static final int SLICE = 50;
 
     /**
      * Farm.
@@ -76,6 +89,11 @@ public final class TkFootprint implements TkRegex {
     @Override
     public Response act(final RqRegex req) throws IOException {
         final String query = new RqHref.Smart(req).single("q", "");
+        final int slice = Math.min(
+            Integer.parseInt(new RqHref.Smart(req).single("slice", "50")),
+            // @checkstyle MagicNumber (1 line)
+            200
+        );
         final int skip = Integer.parseInt(
             new RqHref.Smart(req).single("skip", "0")
         );
@@ -85,59 +103,90 @@ public final class TkFootprint implements TkRegex {
         } else {
             json = BasicDBObject.parse(query);
         }
-        return new RsPage(
-            this.farm,
-            "/xsl/footprint.xsl",
-            req,
-            () -> {
-                final Project project = new RqProject(this.farm, req, "PO");
-                final Collection<Document> docs;
-                try (final Footprint footprint =
-                    new Footprint(this.farm, project)) {
-                    docs = new SolidList<>(
-                        footprint.collection()
-                            .find(
-                                Filters.and(
-                                    Filters.eq("project", project.pid()),
-                                    json
-                                )
+        final Scalar<XeSource> source = () -> {
+            final Project project = new RqProject(this.farm, req, "PO");
+            final Collection<Document> docs;
+            try (final Footprint footprint =
+                new Footprint(this.farm, project)) {
+                docs = new SolidList<>(
+                    footprint.collection()
+                        .find(
+                            Filters.and(
+                                Filters.eq("project", project.pid()),
+                                json
                             )
-                            .sort(Sorts.descending("created"))
-                            .limit(TkFootprint.SLICE)
-                            .skip(skip)
-                    );
-                    docs.size();
-                }
-                final Catalog catalog = new Catalog(this.farm).bootstrap();
-                final String url = String.format(
-                    "?q=%s&skip=", URLEncoder.encode(query, "UTF-8")
-                );
-                return new XeChain(
-                    new XeAppend("project", project.pid()),
-                    new XeAppend("title", catalog.title(project.pid())),
-                    new XeAppend("query", query),
-                    new XeAppend("skip", Integer.toString(skip)),
-                    new XeWhen(
-                        skip >= TkFootprint.SLICE,
-                        new XeLink(
-                            "back",
-                            String.format("%s%d", url, skip - TkFootprint.SLICE)
                         )
-                    ),
-                    new XeWhen(
-                        !docs.isEmpty(),
-                        new XeLink(
-                            "next",
-                            String.format("%s%d", url, skip + TkFootprint.SLICE)
-                        )
-                    ),
-                    new XeAppend(
-                        "claims",
-                        new XeTransform<>(docs, TkFootprint::toSource)
-                    )
+                        .sort(Sorts.descending("created"))
+                        .limit(slice)
+                        .skip(skip)
                 );
+                docs.size();
             }
-        );
+            final Catalog catalog = new Catalog(this.farm).bootstrap();
+            final String url = String.format(
+                "?q=%s&slice=%d&skip=",
+                URLEncoder.encode(query, "UTF-8"),
+                slice
+            );
+            return new XeChain(
+                new XeAppend("project", project.pid()),
+                new XeAppend("title", catalog.title(project.pid())),
+                new XeAppend("query", query),
+                new XeAppend("skip", Integer.toString(skip)),
+                new XeLink(
+                    "plain",
+                    String.format("%s%d?format=plain", url, skip)
+                ),
+                new XeWhen(
+                    skip >= slice,
+                    new XeLink(
+                        "back",
+                        String.format("%s%d", url, skip - slice)
+                    )
+                ),
+                new XeWhen(
+                    !docs.isEmpty(),
+                    new XeLink(
+                        "next",
+                        String.format("%s%d", url, skip + slice)
+                    )
+                ),
+                new XeAppend(
+                    "claims",
+                    new XeTransform<>(docs, TkFootprint::toSource)
+                )
+            );
+        };
+        final String format = new RqHref.Smart(req).single("format", "")
+            .toLowerCase(Locale.ENGLISH);
+        final Response response;
+        if (format.isEmpty()) {
+            response = new RsPage(
+                this.farm,
+                "/xsl/footprint.xsl",
+                req,
+                source
+            );
+        } else {
+            final XML xml = new XMLDocument(
+                new Xembler(
+                    new Directives().add("footprint").append(
+                        new IoCheckedScalar<>(source).value().toXembly()
+                    )
+                ).xmlQuietly()
+            );
+            response = new RsWithType(
+                new RsWithBody(
+                    XSLDocument.make(
+                        TkFootprint.class.getResourceAsStream(
+                            String.format("footprint-%s.xsl", format)
+                        )
+                    ).applyTo(xml)
+                ),
+                String.format("text/%s", format)
+            );
+        }
+        return response;
     }
 
     /**
