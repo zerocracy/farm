6d76fe6b682d90faef46593d05f43f29d90ceb79
Yegor Bugayenko <yegor256@gmail.com>
2018-04-06 17:58:05 +0300

    #882 TkSoftForward

---
+++ src/main/java/com/zerocracy/tk/TkSoftForward.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.tk;
+
+import com.zerocracy.Par;
+import com.zerocracy.SoftException;
+import java.util.logging.Level;
+import org.takes.Take;
+import org.takes.facets.flash.RsFlash;
+import org.takes.facets.forward.RsForward;
+import org.takes.tk.TkWrap;
+
+/**
+ * Forward to home on soft exception.
+ *
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @since 0.22
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+final class TkSoftForward extends TkWrap {
+
+    /**
+     * Ctor.
+     * @param take Take
+     */
+    @SuppressWarnings("PMD.PreserveStackTrace")
+    TkSoftForward(final Take take) {
+        super(request -> {
+            try {
+                return take.act(request);
+            } catch (final SoftException ex) {
+                throw new RsForward(
+                    new RsFlash(
+                        new Par.ToText(ex.getLocalizedMessage()).toString(),
+                        Level.SEVERE
+                    )
+                );
+            }
+        });
+    }
+
+}
