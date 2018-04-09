aa90f6e96a67af3fbf657545054546518a1ad9ea
g4s8 <g4s8.public@gmail.com>
2018-04-09 15:46:37 +0300

    #716 - failing build in BundleTest

--- src/main/resources/com/zerocracy/stk/pm/staff/boost/set_boost.groovy
+++ src/main/resources/com/zerocracy/stk/pm/staff/boost/set_boost.groovy
@@ -17,29 +17,11 @@
 package com.zerocracy.stk.pm.staff.boost
 
 import com.jcabi.xml.XML
-import com.zerocracy.Par
 import com.zerocracy.Project
-import com.zerocracy.SoftException
 import com.zerocracy.farm.Assume
-import com.zerocracy.pm.ClaimIn
-import com.zerocracy.pm.cost.Boosts
 
 def exec(Project project, XML xml) {
   new Assume(project, xml).notPmo()
   new Assume(project, xml).type('Set boost')
-  ClaimIn claim = new ClaimIn(xml)
-  int factor = Integer.valueOf(claim.param('factor').replaceAll('x$', ''))
-  String job = claim.param('job')
-  Boosts boosts = new Boosts(project).bootstrap()
-  if (boosts.factor(job) == factor) {
-    throw new SoftException(
-      new Par(
-        'Current boost factor of %s is %dx, nothing changed'
-      ).say(job, factor)
-    )
-  }
-  boosts.boost(job, factor)
-  claim.reply(
-    new Par('Boost %dx was set for %s').say(factor, job)
-  ).postTo(project)
+  throw new Exception("?")
 }
