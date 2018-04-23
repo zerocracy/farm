581f83a93b5fb8e691f19dbe4b5ba5573b5d8a80
Yegor Bugayenko <yegor256@gmail.com>
2018-04-23 13:43:00 +0300

    #858 notify when ARC goes away

---
+++ src/main/resources/com/zerocracy/stk/pm/staff/roles/notify_jobs_when_arc_resigns.groovy
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.stk.pm.staff.roles
+
+import com.jcabi.xml.XML
+import com.zerocracy.Par
+import com.zerocracy.Project
+import com.zerocracy.farm.Assume
+import com.zerocracy.pm.ClaimIn
+import com.zerocracy.pm.in.Orders
+import com.zerocracy.pm.staff.Roles
+
+def exec(Project project, XML xml) {
+  new Assume(project, xml).notPmo()
+  new Assume(project, xml).type('Role was resigned')
+  ClaimIn claim = new ClaimIn(xml)
+  String role = claim.param('role')
+  if (role != 'ARC') {
+    return
+  }
+  String login = claim.param('login')
+  Roles roles = new Roles(project).bootstrap()
+  String arc = roles.findByRole('ARC')[0]
+  Orders orders = new Orders(project).bootstrap()
+  orders.iterate().each { job ->
+    claim.copy()
+      .type('Notify job')
+      .token("job;${job}")
+      .param(
+        'message', new Par(
+          'The architect of the project has changed;',
+          '@%s is not at this role anymore;',
+          '@%s is the architect now'
+        ).say(login, arc)
+      )
+      .postTo(project)
+  }
+}
