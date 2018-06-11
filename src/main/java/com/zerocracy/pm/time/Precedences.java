8bb12463c42cbed35ff6ad64d4593ef39a8124f0
Mihai A <amihaiemil@gmail.com>
2018-06-11 08:20:03 +0300

    Update Precedences.java

--- src/main/java/com/zerocracy/pm/time/Precedences.java
+++ src/main/java/com/zerocracy/pm/time/Precedences.java
@@ -108,7 +108,6 @@ public final class Precedences {
                     .add("successor")
                     .attr("type", suctype)
                     .set(successor)
-                    .up()
             );
         }
     }
