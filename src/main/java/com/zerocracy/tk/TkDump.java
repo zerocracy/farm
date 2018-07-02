ddc730f366765e4f5acf7ba1b5e172283f74af31
Roman Proshin <roman@proshin.org>
2018-07-02 21:03:30 +0300

    #765: Use HeapDump to get heapdump file's content from S3

--- src/main/java/com/zerocracy/tk/TkDump.java
+++ src/main/java/com/zerocracy/tk/TkDump.java
@@ -16,15 +16,13 @@
  */
 package com.zerocracy.tk;
 
+import com.zerocracy.entry.ExtBucket;
+import com.zerocracy.entry.HeapDump;
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.logging.Level;
 import org.takes.Request;
 import org.takes.Response;
 import org.takes.Take;
-import org.takes.facets.forward.RsForward;
 import org.takes.rs.RsWithBody;
 import org.takes.rs.RsWithType;
 
@@ -34,27 +32,14 @@ import org.takes.rs.RsWithType;
  * @author Kirill (g4s8.public@gmail.com)
  * @version $Id$
  * @since 0.20
- * @todo #400:30min Should use HeapDump to get contents from S3,
- *  rather than from the server file, as it might be deleted and won't be
- *  available anymore.
- * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  */
 public final class TkDump implements Take {
     @Override
     public Response act(final Request request) throws IOException {
-        final File file = new File("./heapdump.hprof");
-        if (!file.exists()) {
-            throw new RsForward(
-                new RsParFlash(
-                    String.format("File doesn't exist: %s", file),
-                    Level.SEVERE
-                )
-            );
-        }
         return new RsWithType(
             new RsWithBody(
                 new BufferedInputStream(
-                    new FileInputStream(file)
+                    new HeapDump(new ExtBucket().value(), "").load()
                 )
             ),
             "application/octet-stream"
