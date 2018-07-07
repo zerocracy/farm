ac943ebe5210d9e7143b049741aab7fa5e4bc7d6
g4s8 <g4s8.public@gmail.com>
2018-07-07 23:18:18 +0300

    #926 - prevent over-election

---
+++ src/main/java/com/zerocracy/pm/staff/votes/VsOverElected.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.pm.staff.votes;
+
+import com.jcabi.log.Logger;
+import com.zerocracy.Project;
+import com.zerocracy.pm.staff.Elections;
+import com.zerocracy.pm.staff.Votes;
+import java.io.IOException;
+import org.cactoos.collection.Filtered;
+import org.cactoos.iterable.LengthOf;
+
+/**
+ * Make it impossible to over-elect one user.
+ *
+ * @author Kirill (g4s8.public@gmail.com)
+ * @version $Id$
+ * @since 0.24
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+public final class VsOverElected implements Votes {
+
+    /**
+     * PMO.
+     */
+    private final Project pkt;
+
+    /**
+     * Elections limit.
+     */
+    private final int limit;
+
+    /**
+     * Ctor.
+     * @param project The project
+     * @param max Max jobs in the agenda
+     */
+    public VsOverElected(final Project project, final int max) {
+        this.pkt = project;
+        this.limit = max;
+    }
+
+    @Override
+    public double take(final String login, final StringBuilder log)
+        throws IOException {
+        final Elections elections = new Elections(this.pkt).bootstrap();
+        final int mine = new LengthOf(
+            new Filtered<>(
+                job -> elections.winner(job).equals(login),
+                elections.jobs()
+            )
+        ).intValue();
+        final double vote;
+        if (mine > this.limit) {
+            log.append(
+                Logger.format(
+                    "%d jobs was elected already, too many (max is %d)",
+                    mine, this.limit
+                )
+            );
+            vote = 1.0d;
+        } else {
+            log.append(
+                Logger.format(
+                    "%d jobs was elected, still enough room (max is %d)",
+                    mine, this.limit
+                )
+            );
+            vote = 0.0d;
+        }
+        return vote;
+    }
+}
---
+++ src/test/java/com/zerocracy/pm/staff/votes/VsOverElectedTest.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.pm.staff.votes;
+
+import com.zerocracy.Project;
+import com.zerocracy.farm.fake.FkProject;
+import com.zerocracy.pm.staff.Elections;
+import com.zerocracy.pm.staff.Votes;
+import org.cactoos.Proc;
+import org.cactoos.iterable.RangeOf;
+import org.cactoos.list.ListOf;
+import org.cactoos.map.MapEntry;
+import org.cactoos.map.MapOf;
+import org.cactoos.scalar.And;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Test case for {@link VsOverElected}.
+ *
+ * @author Kirill (g4s8.public@gmail.com)
+ * @version $Id$
+ * @since 0.24
+ * @checkstyle JavadocMethodCheck (500 lines)
+ * @checkstyle MagicNumberCheck (500 lines)
+ */
+public final class VsOverElectedTest {
+    @Test
+    public void voteForOverElected() throws Exception {
+        final int max = 9;
+        final FkProject pkt = new FkProject();
+        final String login = "user1";
+        new VsOverElectedTest.Elect(pkt, max + 1).exec(login);
+        MatcherAssert.assertThat(
+            new VsOverElected(pkt, max).take(login, new StringBuilder(1)),
+            Matchers.closeTo(1.0, 0.001)
+        );
+    }
+
+    @Test
+    public void ignoreForNotOverElected() throws Exception {
+        final int max = 4;
+        final FkProject pkt = new FkProject();
+        final String login = "user2";
+        new VsOverElectedTest.Elect(pkt, max).exec(login);
+        MatcherAssert.assertThat(
+            new VsOverElected(pkt, max).take(login, new StringBuilder(1)),
+            Matchers.closeTo(0.0, 0.001)
+        );
+    }
+
+    /**
+     * Fake votes.
+     */
+    private static final class VsFake implements Votes {
+        @Override
+        public double take(final String login, final StringBuilder log) {
+            return 1.0;
+        }
+    }
+
+    /**
+     * Elect a performer multiple times.
+     */
+    private static final class Elect implements Proc<String> {
+
+        /**
+         * Project.
+         */
+        private final Project pkt;
+
+        /**
+         * Times to elect.
+         */
+        private final int cnt;
+
+        /**
+         * Ctor.
+         *
+         * @param project Project
+         * @param count Times to elect
+         */
+        private Elect(final Project project, final int count) {
+            this.pkt = project;
+            this.cnt = count;
+        }
+
+        @Override
+        public void exec(final String login) throws Exception {
+            final Elections elections = new Elections(this.pkt).bootstrap();
+            new And(
+                (Integer num) -> elections.elect(
+                    String.format("gh:test/test#%d", num),
+                    new ListOf<>(login),
+                    new MapOf<Votes, Integer>(
+                        new MapEntry<>(
+                            new VsOverElectedTest.VsFake(),
+                            1
+                        )
+                    )
+                ),
+                new RangeOf<>(1, this.cnt, num -> num + 1)
+            ).value();
+        }
+    }
+}
