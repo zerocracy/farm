0b335bbf22e2d32510653f949ae39cc0672225e9
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2018-07-09 09:09:48 +0800

    #1123 Re-enable Jobs and Agenda display in Team page

--- src/main/java/com/zerocracy/tk/TkTeam.java
+++ src/main/java/com/zerocracy/tk/TkTeam.java
@@ -42,10 +42,7 @@ import org.takes.rs.xe.XeWhen;
  * @author Yegor Bugayenko (yegor256@gmail.com)
  * @version $Id$
  * @since 0.19
- * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
- * @todo #1121:30min Speed and Agenda is skipped now because it
- *  causes slow page loading. We have to fix #1121 bug and show
- *  it on page as before.
+ * @checkstyle ClassDataAbstractionCouplingCheck (3 lines)
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public final class TkTeam implements Take {
@@ -100,8 +97,8 @@ public final class TkTeam implements Take {
                 new XeAppend("login", node.xpath("@id").get(0)),
                 new XeAppend("mentor", node.xpath("mentor/text()").get(0)),
                 new XeAppend("awards", node.xpath("reputation/text()").get(0)),
-                new XeAppend("speed", "0.0"),
-                new XeAppend("agenda", "-"),
+                new XeAppend("speed", node.xpath("speed/text()").get(0)),
+                new XeAppend("agenda", node.xpath("jobs/text()").get(0)),
                 new XeAppend(
                     "projects",
                     Integer.toString(
