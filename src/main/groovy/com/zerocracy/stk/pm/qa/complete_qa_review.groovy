6ebb6cf11b9e8b1292897515e0dde8f066934c46
g4s8 <g4s8.public@gmail.com>
2018-07-15 13:21:17 +0300

    #1395 - codenarc

--- src/main/groovy/com/zerocracy/stk/pm/qa/complete_qa_review.groovy
+++ src/main/groovy/com/zerocracy/stk/pm/qa/complete_qa_review.groovy
@@ -44,7 +44,7 @@ def exec(Project project, XML xml) {
       new Par('Thanks, but quality review is not required in this job').say()
     )
   }
-  int bonus = new Policy().get("31.bonus-minutes", 5)
+  int bonus = new Policy().get('31.bonus-minutes', 5)
   ClaimOut out = reviews.remove(job, quality == 'good' ? bonus : 0, claim.copy())
   if (quality == 'bad') {
     claim.copy()
