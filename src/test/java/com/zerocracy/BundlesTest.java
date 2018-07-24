5747dc3a010dfcf50b31ff54760606a72396dc21
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-07-24 16:21:52 +0200

    #1116 fixing build

--- src/test/java/com/zerocracy/BundlesTest.java
+++ src/test/java/com/zerocracy/BundlesTest.java
@@ -150,7 +150,7 @@ public final class BundlesTest {
                 },
                 new Sorted<>(
                     new Reflections(
-                        "com.zerocracy.bundles.dont_resign_on_delay_unfunded_project", new ResourcesScanner()
+                        "com.zerocracy.bundles.resign_on_delay", new ResourcesScanner()
                     ).getResources(p -> p.endsWith("claims.xml"))
                 )
             )
