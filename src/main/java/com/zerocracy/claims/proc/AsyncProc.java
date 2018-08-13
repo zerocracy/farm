274f1510fe47a45118ecf52c43033f6b4dd66663
g4s8 <g4s8.public@gmail.com>
2018-08-13 14:38:24 +0300

    Revert "#1567 - fixed UnsupportedOperationException"
    This reverts commit 66b08afb137bf0de0bea439c7215ba55c156ce05.

--- src/main/java/com/zerocracy/claims/proc/AsyncProc.java
+++ src/main/java/com/zerocracy/claims/proc/AsyncProc.java
@@ -29,9 +29,7 @@ import com.zerocracy.entry.ExtSqs;
 import com.zerocracy.shutdown.ShutdownFarm;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
@@ -160,15 +158,11 @@ public final class AsyncProc implements Proc<List<Message>> {
                                 input.size()
                             );
                             final Iterator<Message> messages = copy.iterator();
-                            final Collection<Message> remove =
-                                new LinkedList<>();
                             while (messages.hasNext()) {
-                                final Message message = messages.next();
-                                this.origin.exec(message);
-                                remove.add(message);
+                                this.origin.exec(messages.next());
+                                messages.remove();
                                 done.countDown();
                             }
-                            copy.removeAll(remove);
                         } finally {
                             if (this.count.decrementAndGet() == 0
                                 && this.shutdown.stopping()) {
