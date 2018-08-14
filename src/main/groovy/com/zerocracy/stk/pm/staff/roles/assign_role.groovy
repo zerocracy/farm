a7b265c44ba6b83ee093b4a3e9bf5083d2ab7d62
Paulo Lobo <pauloeduardolobo@gmail.com>
2018-08-14 11:28:50 -0300

    For #1100: Review corrections.

--- src/main/groovy/com/zerocracy/stk/pm/staff/roles/assign_role.groovy
+++ src/main/groovy/com/zerocracy/stk/pm/staff/roles/assign_role.groovy
@@ -77,17 +77,16 @@ def exec(Project project, XML xml) {
       'Role %s was already assigned to @%s; '
     ).say(role, login)
   } else {
+    String text = 'Role %s was successfully assigned to @%s, see [full list](/a/%s?a=pm/staff/roles) of roles; '
     if (role == 'DEV') {
       claim.copy()
         .type('Assign Role')
         .param('role', 'REV')
         .postTo(new ClaimsOf(farm, project))
+    } else if (role == 'REV') {
+        text = text + 'If you don\'t want this user as \'REV\', use `resign REV` command; '
     }
     roles.assign(login, role)
-    String text = 'Role %s was successfully assigned to @%s, see [full list](/a/%s?a=pm/staff/roles) of roles; '
-    if (role == 'REV') {
-        text = text + 'If you don\'t want this user as \'REV\', use resign REV command'
-    }
     msg = new Par(
         text
     ).say(role, login, project.pid())
--- src/test/resources/com/zerocracy/bundles/assign_role/_after.groovy
+++ src/test/resources/com/zerocracy/bundles/assign_role/_after.groovy
@@ -17,13 +17,21 @@
 package com.zerocracy.bundles.assign_role
 
 import com.jcabi.xml.XML
+import com.mongodb.client.model.Filters
+import com.zerocracy.Farm
+import com.zerocracy.Par
 import com.zerocracy.Project
+import com.zerocracy.claims.Footprint
 import com.zerocracy.pm.staff.Roles
+import org.cactoos.text.JoinedText
 import org.hamcrest.MatcherAssert
+import org.hamcrest.collection.IsEmptyIterable
 import org.hamcrest.core.IsCollectionContaining
 import org.hamcrest.core.IsEqual
+import org.hamcrest.core.IsNot
 
 def exec(Project project, XML xml) {
+  Farm farm = binding.variables.farm
   Roles roles = new Roles(project).bootstrap()
   MatcherAssert.assertThat(
     'DEV role was not assigned to g4s8',
@@ -37,6 +45,48 @@ def exec(Project project, XML xml) {
     roles.allRoles('g4s8'),
     new IsCollectionContaining<>(
       new IsEqual('REV')
+    )
+  )
+  MatcherAssert.assertThat(
+    'Messages not sent to ARC',
+    new Footprint(farm, project).collection().find(
+      Filters.and(
+        Filters.eq('project', project.pid()),
+        Filters.eq('type', 'Notify test'),
+        Filters.or(
+          Filters.eq(
+            'message',
+            new JoinedText(
+              '',
+              'Role `DEV` was successfully assigned to @g4s8[/z]',
+              '(https://www.0crat.com/u/g4s8), see [full list]',
+              '(https://www.0crat.com/a/ASSIGNROL?a=pm/staff/roles) of roles; ',
+              'hourly rate of @g4s8[/z](https://www.0crat.com/u/g4s8) is not ',
+              'set, the user will receive no money for task completion; ',
+              'if you want to assign an hourly rate, say `assign DEV g4s8 $25`',
+              ', for example'
+            ).asString()
+          ),
+          Filters.eq(
+            'message',
+            new JoinedText(
+              '',
+              'Role `REV` was successfully assigned to @g4s8[/z]',
+              '(https://www.0crat.com/u/g4s8), see [full list]',
+              '(https://www.0crat.com/a/ASSIGNROL?a=pm/staff/roles) of roles; ',
+              'If you don\'t want this user as \'REV\', use ',
+              '`resign REV` command; ',
+              'hourly rate of @g4s8[/z](https://www.0crat.com/u/g4s8) is not ',
+              'set, the user will receive no money for task completion; ',
+              'if you want to assign an hourly rate, say `assign REV g4s8 $25`',
+              ', for example'
+            ).asString()
+          )
+        )
+      )
+    ),
+    new IsNot<>(
+      new IsEmptyIterable()
     )
   )
   // @todo #989:30min MkGithub can't create private repository,
