821953f07f85102a897017540580a9a9a613b37b
g4s8 <g4s8.public@gmail.com>
2018-08-18 20:36:52 +0300

    #1076 - temporary disabled recharge

--- src/main/groovy/com/zerocracy/stk/pm/cost/funding/recharge_on_deficit.groovy
+++ src/main/groovy/com/zerocracy/stk/pm/cost/funding/recharge_on_deficit.groovy
@@ -37,7 +37,7 @@ import com.zerocracy.pmo.recharge.Recharge
  */
 def exec(Project project, XML xml) {
   new Assume(project, xml).notPmo()
-  new Assume(project, xml).type('Make payment', 'Ping hourly')
+//  new Assume(project, xml).type('Make payment', 'Ping hourly')
   ClaimIn claim = new ClaimIn(xml)
   Farm farm = binding.variables.farm
   Recharge recharge = new Recharge(farm, project.pid())
