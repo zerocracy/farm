a6b778e7c930dc8d4626311744be42965a62f2fc
g4s8 <g4s8.public@gmail.com>
2018-08-27 19:19:57 +0300

    #1663 - remove invalid elections

---
+++ src/main/groovy/com/zerocracy/stk/pm/staff/elections/remove_stale_elections.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.stk.pm.staff.elections
+
+import com.jcabi.xml.XML
+import com.zerocracy.Project
+import com.zerocracy.farm.Assume
+import com.zerocracy.pm.in.Orders
+import com.zerocracy.pm.scope.Wbs
+import com.zerocracy.pm.staff.ElectionResult
+import com.zerocracy.pm.staff.Elections
+
+/**
+ * Remove all invalid elections: if election result is not successful or
+ * job was assigned to performer or job was removed from WBS.
+ *
+ * @param project Project
+ * @param xml Claim
+ */
+def exec(Project project, XML xml) {
+  new Assume(project, xml).notPmo()
+  new Assume(project, xml).type('Ping hourly')
+  Elections elections = new Elections(project).bootstrap()
+  Orders orders = new Orders(project).bootstrap()
+  Wbs wbs = new Wbs(project).bootstrap()
+  elections.jobs().each {
+    ElectionResult result = elections.result(it)
+    if (!result.elected() || orders.assigned(it) || !wbs.exists(it)) {
+      elections.remove(it)
+    }
+  }
+}
