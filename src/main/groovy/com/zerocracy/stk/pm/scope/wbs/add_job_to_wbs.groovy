2a38485dc25dde2f3b68e9cea5878fedde3a860e
Paulo Lobo <pauloeduardolobo@gmail.com>
2018-09-06 15:44:46 -0300

    For #770: Corrected codenarc issues.

--- src/main/groovy/com/zerocracy/stk/pm/scope/wbs/add_job_to_wbs.groovy
+++ src/main/groovy/com/zerocracy/stk/pm/scope/wbs/add_job_to_wbs.groovy
@@ -26,7 +26,6 @@ import com.zerocracy.SoftException
 import com.zerocracy.entry.ClaimsOf
 import com.zerocracy.entry.ExtGithub
 import com.zerocracy.farm.Assume
-import com.zerocracy.farm.props.Props
 import com.zerocracy.claims.ClaimIn
 import com.zerocracy.pm.scope.Wbs
 import com.zerocracy.pm.staff.Roles
