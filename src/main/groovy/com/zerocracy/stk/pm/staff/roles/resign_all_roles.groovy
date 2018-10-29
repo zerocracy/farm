a536f61616ef26459fc3893cc5973fe060809e5d
Kirill <g4s8.public@gmail.com>
2018-10-29 22:28:09 +0300

    #1779 - fixed resign_all_roles error

--- src/main/groovy/com/zerocracy/stk/pm/staff/roles/resign_all_roles.groovy
+++ src/main/groovy/com/zerocracy/stk/pm/staff/roles/resign_all_roles.groovy
@@ -20,6 +20,7 @@ import com.jcabi.xml.XML
 import com.zerocracy.Farm
 import com.zerocracy.Par
 import com.zerocracy.Project
+import com.zerocracy.SoftException
 import com.zerocracy.entry.ClaimsOf
 import com.zerocracy.farm.Assume
 import com.zerocracy.claims.ClaimIn
@@ -38,7 +39,11 @@ def exec(Project project, XML xml) {
     )
   ).postTo(new ClaimsOf(farm, project))
   roles.allRoles(login).each { role ->
-    roles.resign(login, role)
+    try {
+      roles.resign(login, role)
+    } catch (SoftException err) {
+      claim.reply(err.message)
+    }
     claim.copy()
       .type('Role was resigned')
       .param('login', login)
