2629455f3dbecdb3df6fed8b57cb6cf7d2842d45
Kirill <g4s8.public@gmail.com>
2019-04-13 22:42:12 +0300

    Revert "#1977 - updated yoti library"
    This reverts commit 2eb80332e2a028719a97289e0bb14095fa0ae0d3.

--- src/main/java/com/zerocracy/tk/profile/TkYoti.java
+++ src/main/java/com/zerocracy/tk/profile/TkYoti.java
@@ -16,7 +16,6 @@
  */
 package com.zerocracy.tk.profile;
 
-import com.yoti.api.client.Date;
 import com.yoti.api.client.FileKeyPairSource;
 import com.yoti.api.client.HumanProfile;
 import com.yoti.api.client.ProfileException;
@@ -81,11 +80,12 @@ public final class TkYoti implements TkRegex {
         } catch (final ProfileException ex) {
             throw new IOException(ex);
         }
-        final Date bdate = profile.getDateOfBirth().getValue();
         final String name = String.format(
             "%s %s %d-%d-%d @Yoti",
             profile.getGivenNames(), profile.getFamilyName(),
-            bdate.getDay(), bdate.getMonth(), bdate.getYear()
+            profile.getDateOfBirth().getDay(),
+            profile.getDateOfBirth().getMonth(),
+            profile.getDateOfBirth().getYear()
         );
         final String user = new RqUser(this.farm, req).value();
         new People(this.farm).bootstrap().details(user, name);
@@ -110,4 +110,5 @@ public final class TkYoti implements TkRegex {
             String.format("/u/%s", user)
         );
     }
+
 }
